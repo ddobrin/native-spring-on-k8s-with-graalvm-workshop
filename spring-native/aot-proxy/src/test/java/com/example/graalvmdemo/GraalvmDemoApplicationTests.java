e6c9c5d004511e94f204276014c568c66845aba7
Dan Dobrin <dan.dobrin@gmail.com>
2021-06-16 17:46:40 -0400

    testing agent

---
+++ spring-native/aot-proxy/src/test/java/com/example/graalvmdemo/GraalvmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graalvmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
