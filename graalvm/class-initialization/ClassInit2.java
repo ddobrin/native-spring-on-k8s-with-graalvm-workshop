af6d49a48876d74f8c2ffd374b14e16705eb9c88
Dan Dobrin <dan.dobrin@gmail.com>
2021-08-10 10:35:09 -0400

    added a second sample for graalvm/class-initialization

---
+++ graalvm/class-initialization/ClassInit2.java
@@ -0,0 +1,36 @@
+import java.nio.charset.*;
+import java.util.UUID;
+
+public class ClassInit2 {
+    public static void main(String[] args) {
+        First.second.printIt();
+    }
+}
+
+class First {
+    public static Second second = new Second();
+
+    // Uncomment this section for part 2 of the exercise
+    public static Thread t;
+
+    static {
+        t = new Thread(()-> {
+            try {
+                System.out.println("Sleep for 10s...");
+                Thread.sleep(10_000);
+                System.out.println("Done...");
+            } catch (Exception e){}
+        });
+        t.start();
+    }
+
+}
+
+class Second {
+    private static final Charset UTF_32_LE = Charset.forName("UTF-32LE");
+
+    public void printIt() {
+        System.out.println("Unicode 32 bit CharSet: " + UTF_32_LE);
+    }
+}
+
